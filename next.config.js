42b87509e65c02a549cd82fa2f64497746924963
Armandorubiogarcia <axmanpalomeque@gmail.com>
2026-02-24 11:51:31 +0100

    Add comments to next.config.js for clarity
    Added comments in Spanish to explain configuration options.

--- next.config.js
+++ next.config.js
@@ -1,9 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   typescript: {
+    // Esto evita que las líneas rojas bloqueen la web
     ignoreBuildErrors: true,
   },
   eslint: {
+    // Esto evita que los avisos de estilo bloqueen la web
     ignoreDuringBuilds: true,
   },
 }
